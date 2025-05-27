0a566e7c02901b0daa3b61c00ae1c73711935268
Artem <artemklymenko892@gmail.com>
2025-05-27 16:23:36 +0300

    Add episode fetching functionality

--- network/src/main/java/com/artemklymenko/network/models/core/client/KtorClient.kt
+++ network/src/main/java/com/artemklymenko/network/models/core/client/KtorClient.kt
@@ -5,6 +5,9 @@ import com.artemklymenko.network.models.data.mappers.toDomainCharacter
 import com.artemklymenko.network.models.domain.DomainCharacter
 import com.artemklymenko.network.models.remote.RemoteCharacter
 import com.artemklymenko.network.models.core.utils.Constants.BASE_URL
+import com.artemklymenko.network.models.data.mappers.toDomainEpisode
+import com.artemklymenko.network.models.domain.DomainEpisode
+import com.artemklymenko.network.models.remote.RemoteEpisode
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
@@ -35,11 +38,44 @@ class KtorClient {
         }
     }
 
+    private var characterCache = mutableMapOf<Int, DomainCharacter>()
+    private var episodeCache = mutableMapOf<Int, DomainEpisode>()
+    private var episodesCache = mutableMapOf<List<Int>, List<DomainEpisode>>()
+
     suspend fun getCharacter(id: Int): ApiOperation<DomainCharacter> {
+        characterCache[id]?.let { return ApiOperation.Success(it) }
         return safeApiCall {
             client.get("character/$id")
                 .body<RemoteCharacter>()
                 .toDomainCharacter()
+                .also { characterCache[id] = it }
+        }
+    }
+
+    private suspend fun getEpisode(episodeId: Int): ApiOperation<DomainEpisode> {
+        episodeCache[episodeId]?.let { return ApiOperation.Success(it) }
+        return safeApiCall {
+            client.get("episode/$episodeId")
+                .body<RemoteEpisode>()
+                .toDomainEpisode()
+                .also { episodeCache[episodeId] = it }
+        }
+    }
+
+    suspend fun getEpisodes(episodesIds: List<Int>): ApiOperation<List<DomainEpisode>> {
+        episodesCache[episodesIds]?.let { return ApiOperation.Success(it) }
+        return if(episodesIds.size == 1) {
+            getEpisode(episodesIds[0]).mapSuccess {
+                listOf(it)
+            }
+        } else {
+            val idsCommaSeparated = episodesIds.joinToString(separator = ",")
+            safeApiCall {
+                client.get("episode/$idsCommaSeparated")
+                    .body<List<RemoteEpisode>>()
+                    .map { it.toDomainEpisode() }
+                    .also { episodesCache[episodesIds] = it }
+            }
         }
     }
 
--- network/src/main/java/com/artemklymenko/network/models/core/utils/ApiOperation.kt
+++ network/src/main/java/com/artemklymenko/network/models/core/utils/ApiOperation.kt
@@ -1,10 +1,18 @@
 package com.artemklymenko.network.models.core.utils
 
+import com.artemklymenko.network.R
 import java.lang.Exception
 
 sealed interface ApiOperation<T>{
     data class Success<T>(val data: T): ApiOperation<T>
     data class Failure<T>(val exception: Exception): ApiOperation<T>
+
+    fun <R> mapSuccess(transform: (T) -> R): ApiOperation<R> {
+        return when(this) {
+            is Success -> Success(transform(data))
+            is Failure -> Failure(exception)
+        }
+    }
 
     fun onSuccess(block: (T) -> Unit): ApiOperation<T> {
         if(this is Success) {
