33c8e5a74777e9f470853c72408a351226d59dc6
Artem <artemklymenko892@gmail.com>
2025-05-29 12:06:36 +0300

    Add more colors to Color.kt

--- app/src/main/java/com/artemklymenko/rickipedia/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/artemklymenko/rickipedia/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.artemklymenko.rickipedia.ui.theme
+package com.artemklymenko.rickipedia.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
